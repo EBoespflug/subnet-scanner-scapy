cd6cd50b7e33a17da18f455f84cabab97c8ecf4b
Boespflug Etienne <etienne.boespflug@gmail.com>
2017-04-07 22:15:57 +0200

    Added documentation for Host.py and subnet_scan.py

--- Host.py
+++ Host.py
@@ -1,8 +1,15 @@
 class Host:
+    """The Host class represent an host on the network.
+    It is composed by it's name and it's addresses (IP & MAC)."""
     def __init__(self, ip, mac, name):
+        """Constructs an Host with the specified IP, MAC and name.
+        @param ip   the IP address of the host.
+        @param mac  the MAC address of the host.
+        @param name the host's name."""
         self.ip = str(ip)
         self.mac = str(mac)
         self.name = str(name)
 
     def __str__(self):
+        """@returns the string representation of the host."""
         return "(" + self.ip + " " + self.mac + " " + self.name + ")"
--- subnet_scanner.py
+++ subnet_scanner.py
@@ -2,24 +2,31 @@ from scapy.all import *
 from Host import *
 
 def resolve_host_name(ip):
-    results = ["unknown"]
+    """Tries to resolve the host name from the specified address.
+    @param ip   the ip of the host.
+    @return the name of the host if the operation succeed, 'unknown' otherwise."""
+    results = []
     try:
         results = socket.gethostbyaddr(ip)
-    except socket.herror: pass
+    except socket.herror: pass # ignore. Nothing to do.
 
-    hostname = "unknown"
+    hostname = "unknown" # default host name.
     if results != "":
         hostname = results[0] # keep only the host name.
     return hostname
 
-def arping(ip_range, timout):
-    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip_range), timeout=timout)
+def arping(ip_range, timeout = 2):
+    """Returns a list of alive Host in the specified ip_range.
+    @param ip_range an range of ip (ex : 192.168.1.*, 192.168.1.2-254).
+    @param timeout  the timeout of the arp paquets.
+    @return the list of alive host found in the specified subnet."""
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_range), timeout=timeout)
 
     hosts = []
     for req, res in ans:
         ip = res.getlayer(ARP).psrc
         mac = res.getlayer(Ether).src
-        name = resolve_host_name(ip)
+        name = resolve_host_name(ip) # try to get the host name.
         hosts.append(Host(ip, mac, name))
 
     return hosts
